73d9cae704813b3d5b49a15035c42f855ce23d68
Rogerio Fernandes <roferrock@gmail.com>
2026-08-21 07:20:34 -0300

    feat: add validation to signup form

--- eslint.config.js
+++ eslint.config.js
@@ -34,12 +34,17 @@ export default [
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
       'react/jsx-no-target-blank': 'off',
+      'react/no-unescaped-entities': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
       'react/prop-types': 'off',
     },
+  },
+  {
+    files: ['src/components/ui/**'],
+    rules: { 'react-refresh/only-export-components': 'off' },
   },
   eslintConfigPrettier,
 ];
