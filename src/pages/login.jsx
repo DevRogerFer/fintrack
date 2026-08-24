52594e4d695c2a97b8198444f2ddaceb36747348
Rogerio Fernandes <roferrock@gmail.com>
2026-08-24 07:18:49 -0300

    feat: add initial login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,41 @@
+import { Link } from 'react-router';
+
+import PasswordInput from '@/components/password-input';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+
 const LoginPage = () => {
-  return <h1>Login page</h1>;
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
+      <Card className="w-[500px]">
+        <CardHeader>
+          <CardTitle>Entre na sua conta</CardTitle>
+          <CardDescription>Insira seus dados abaixo:</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <Input placeholder="Digite seu e-mail"></Input>
+          <PasswordInput placeholder="Digite sua senha" />
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Fazer login</Button>
+        </CardFooter>
+      </Card>
+      <div className="flex items-center justify-center">
+        <p className="text-center opacity-50">Ainda não possui conta?</p>
+        <Button variant="link" asChild>
+          <Link to="signup">Crie agora</Link>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
