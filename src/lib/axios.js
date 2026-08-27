8a4a5c1988e7a20d99490604232e0c089b11aaad
Rogerio Fernandes <roferrock@gmail.com>
2026-08-27 06:32:13 -0300

    chore: add axios setup

---
+++ src/lib/axios.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com/docs',
+  //   https://fullstackclub-finance-dashboard-api.onrender.com
+});
